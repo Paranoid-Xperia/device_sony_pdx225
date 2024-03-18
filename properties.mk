9c2f663fe6d25ef2ec966bf36b07727bce47729b
Mohammad Sayful Islam <Sayf.mohammad01@gmail.com>
2024-03-18 08:15:33 +0000

    pdx225: cleanup props

--- properties.mk
+++ properties.mk
@@ -65,8 +65,6 @@ persist.sys.fuse.passthrough.enable=true
 # Graphics
 PRODUCT_VENDOR_PROPERTIES += \
 ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.has_wide_color_display=false \
-ro.surface_flinger.has_HDR_display=false \
 ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 ro.surface_flinger.max_virtual_display_dimension=4096 \
 ro.surface_flinger.protected_contents=true \
